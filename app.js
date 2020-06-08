f29dee7766e10061b5b27956bac71ef8c69db753
Michelle Nesbitt <mn218@nerdymail.com>
2020-06-08 08:00:44 -0400

    mn as of monday

--- app.js
+++ app.js
@@ -32,120 +32,120 @@ function QuizModel() {
 
     //set the questions values in the array
     self.questions([
-    new Question({
-        id: 1, 
-        question: "Which of the following is not an escape sequence in JavaScript?",
-        A: "\\",
-        B: "\\t",
-        C: "\\r",
-        D: "\\\'",
-        correct: "A",
-        type: "Easy"
-    }),
-    new Question({
-        id: 2, 
-        question: "Which of the following are used to enclose function parameters?",
-        A: "{}",
-        B: "[]",
-        C: "()",
-        D: "<>",
-        correct: "C",
-        type: "Medium"
-    }),
-    new Question({
-        id: 3, 
-        question: "Which method of an Array calls a function for each element in the array?",
-        A: "forEach()",
-        B: "every()",
-        C: "forEvery()",
-        D: "each()",
-        correct: "A",
-        type: "Difficult"
-    }),
-    new Question({
-        id: 4, 
-        question: "Under which HTML tag is JavaScript code written?",
-        A: "<javascript>",
-        B: "<jscode>", 
-        C: "<js>",
-        D: "<script>",
-        correct: "D",
-        type: "Difficult"
-    }),
-    new Question({
-        id: 5, 
-        question: "How do you call myFunction() in JavaScript?",
-        A: "Call myFunc()",
-        B: "myFunction()",
-        C: "Call function myFunction()",
-        D: "function myFunction",
-        correct: "B",
-        type: "Easy"
-    }),
-    new Question({   
-        id: 6, 
-        question: "Which of the following is not valid in JavaScript?",
-        A: "const name = 'John';",
-        B: "var name = 'John';",
-        C: "console.log(name);",
-        D: "int number = 10;",
-        correct: "D",
-        type: "Difficult"
+        new Question({
+            id: 1, 
+            question: "Which of the following is not an escape sequence in JavaScript?",
+            A: "\\",
+            B: "\\t",
+            C: "\\r",
+            D: "\\\'",
+            correct: "A",
+            type: "Easy"
         }),
-    new Question({   
-        id: 7, 
-        question: "Which assignment operator is invalid in JavaScript?",
-        A: "x **= y;",
-        B: "x == y;",
-        C: "x %= y;",
-        D: "x -= y;",
-        correct: "B",
-        type: "Medium"
+        new Question({
+            id: 2, 
+            question: "Which of the following are used to enclose function parameters?",
+            A: "{}",
+            B: "[]",
+            C: "()",
+            D: "<>",
+            correct: "C",
+            type: "Medium"
         }),
-    new Question({ 
-        id: 8, 
-        question: "JavaScript is a ________-side scripting language.",
-        A: "Server",
-        B: "ISP",
-        C: "Browser",
-        D: "All of the above.",
-        correct: "C",
-        type: "Easy"
+        new Question({
+            id: 3, 
+            question: "Which method of an Array calls a function for each element in the array?",
+            A: "forEach()",
+            B: "every()",
+            C: "forEvery()",
+            D: "each()",
+            correct: "A",
+            type: "Difficult"
         }),
-    new Question({   
-        id: 9, 
-        question: "Which of the following JavaScript statements displays 'Hello World' in an alert box?",
-        A: "alerttext(“HelloWorld!”);",
-        B: "alertmsg(“HelloWorld!”);",
-        C: "msgbox(“HelloWorld!”);",
-        D: "alert(“HelloWorld!”);",
-        correct: "D",
-        type: "Difficult"
+        new Question({
+            id: 4, 
+            question: "Under which HTML tag is JavaScript code written?",
+            A: "<javascript>",
+            B: "<jscode>", 
+            C: "<js>",
+            D: "<script>",
+            correct: "D",
+            type: "Difficult"
         }),
-    new Question({
-        id: 10, 
-        question: "What is the correct syntax for adding a comment in JavaScript?",
-        A: "//This is a comment",
-        B: "<!–-This is a comment-->",
-        C: "-*This is a comment",
-        D: "!This is a comment",
-        correct: "A",
-        type: "Medium"
-    }), ])
+        new Question({
+            id: 5, 
+            question: "How do you call myFunction() in JavaScript?",
+            A: "Call myFunc()",
+            B: "myFunction()",
+            C: "Call function myFunction()",
+            D: "function myFunction",
+            correct: "B",
+            type: "Easy"
+        }),
+        new Question({   
+            id: 6, 
+            question: "Which of the following is not valid in JavaScript?",
+            A: "const name = 'John';",
+            B: "var name = 'John';",
+            C: "console.log(name);",
+            D: "int number = 10;",
+            correct: "D",
+            type: "Difficult"
+            }),
+        new Question({   
+            id: 7, 
+            question: "Which assignment operator is invalid in JavaScript?",
+            A: "x **= y;",
+            B: "x == y;",
+            C: "x %= y;",
+            D: "x -= y;",
+            correct: "B",
+            type: "Medium"
+        }),
+        new Question({ 
+            id: 8, 
+            question: "JavaScript is a ________-side scripting language.",
+            A: "Server",
+            B: "ISP",
+            C: "Browser",
+            D: "All of the above.",
+            correct: "C",
+            type: "Easy"
+        }),
+        new Question({   
+            id: 9, 
+            question: "Which of the following JavaScript statements displays 'Hello World' in an alert box?",
+            A: "alerttext(“HelloWorld!”);",
+            B: "alertmsg(“HelloWorld!”);",
+            C: "msgbox(“HelloWorld!”);",
+            D: "alert(“HelloWorld!”);",
+            correct: "D",
+            type: "Difficult"
+        }),
+        new Question({
+            id: 10, 
+            question: "What is the correct syntax for adding a comment in JavaScript?",
+            A: "//This is a comment",
+            B: "<!–-This is a comment-->",
+            C: "-*This is a comment",
+            D: "!This is a comment",
+            correct: "A",
+            type: "Medium"           
+        }), ])
 
-    self.filterQuestionsById = ko.computed(function () {
-        if (!self.currentIndex()) {
-            return self.questions();
-        } else {
-            return ko.utils.arrayFilter(self.questions(), function (ques) {
-                return ques.id == self.currentIndex();
-            });
+        self.filterQuestionsById = ko.computed(function () {
+            if (!self.currentIndex()) {
+                return self.questions();
+            } else {
+                return ko.utils.arrayFilter(self.questions(), function (ques) {
+                    return ques.id == self.currentIndex();
+                });
+            }
+        });
+
+        self.filter = function(idNum) {
+            self.currentIndex(idNum);
         }
-    });
-
-    self.filter = function(idNum) {
-        self.currentIndex(idNum);
-    }
 }
 
 ko.applyBindings(new QuizModel())
