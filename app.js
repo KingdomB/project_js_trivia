35e77d8e92aca956d1ecc6e1f1749c2346a70495
Michelle Nesbitt <mn218@nerdymail.com>
2020-06-08 07:29:31 -0400

    729a

--- app.js
+++ app.js
@@ -11,102 +11,141 @@ function hide(){
         document.getElementById("answers").style.visibility = "visible";
     }
 }
- //Questions array. See assignments/examples for this week to see 
-            //why i set these up this way
-function QuestionViewModel(){
 
+function Question(data) {
+    this.id = data.id;
+    this.question = data.question;
+    this.A = data.A;
+    this.B = data.B;
+    this.C = data.C;
+    this.D = data.D;
+    this.correct = data.correct;
+    this.type = data.type;    
+}
+
+function QuizModel() {
     var self = this;
-    //This holds the current record which can be changed.
-    self.currentRecord = ko.observable(0);
+    
+    self.questions = ko.observableArray([]);
 
-    self.questions=ko.observableArray([
-        //Question 1: index 0
-        {question: "Which of the following is not an escape sequence in JavaScript?",
+    self.currentIndex = ko.observable(); // store the current filter
+
+    //set the questions values in the array
+    self.questions([
+    new Question({
+        id: 1, 
+        question: "Which of the following is not an escape sequence in JavaScript?",
         A: "\\",
         B: "\\t",
         C: "\\r",
         D: "\\\'",
-        correct: A
-        },
-        //Question 2, index 1
-        {question: "Which of the following are used to enclose function parameters?",
+        correct: "A",
+        type: "Easy"
+    }),
+    new Question({
+        id: 2, 
+        question: "Which of the following are used to enclose function parameters?",
         A: "{}",
         B: "[]",
         C: "()",
         D: "<>",
-        correct: C
-        },
-        //Question 3, index 2
-        {question: "Which method of an Array calls a function for each element in the array?",
+        correct: "C",
+        type: "Medium"
+    }),
+    new Question({
+        id: 3, 
+        question: "Which method of an Array calls a function for each element in the array?",
         A: "forEach()",
         B: "every()",
         C: "forEvery()",
         D: "each()",
-        correct: A
-        },
-        //Question 4, index 3
-        {question: "Under which HTML tag is JavaScript code written?",
+        correct: "A",
+        type: "Difficult"
+    }),
+    new Question({
+        id: 4, 
+        question: "Under which HTML tag is JavaScript code written?",
         A: "<javascript>",
         B: "<jscode>", 
         C: "<js>",
         D: "<script>",
-        correct: D
-        },
-        //Question 5, index 4
-        {question: "How do you call myFunction() in JavaScript?",
+        correct: "D",
+        type: "Difficult"
+    }),
+    new Question({
+        id: 5, 
+        question: "How do you call myFunction() in JavaScript?",
         A: "Call myFunc()",
         B: "myFunction()",
         C: "Call function myFunction()",
         D: "function myFunction",
-        correct: B
-        },
-        //Question 6, index 5
-        {question: "Which of the following is not valid in JavaScript?",
+        correct: "B",
+        type: "Easy"
+    }),
+    new Question({   
+        id: 6, 
+        question: "Which of the following is not valid in JavaScript?",
         A: "const name = 'John';",
         B: "var name = 'John';",
         C: "console.log(name);",
         D: "int number = 10;",
-        correct: D
-        },
-        //Question 7, index 6
-        {question: "Which assignment operator is invalid in JavaScript?",
+        correct: "D",
+        type: "Difficult"
+        }),
+    new Question({   
+        id: 7, 
+        question: "Which assignment operator is invalid in JavaScript?",
         A: "x **= y;",
         B: "x == y;",
         C: "x %= y;",
         D: "x -= y;",
-        correct: B
-        },
-        //Question 8, index 7
-        {question: "JavaScript is a ________-side scripting language.",
+        correct: "B",
+        type: "Medium"
+        }),
+    new Question({ 
+        id: 8, 
+        question: "JavaScript is a ________-side scripting language.",
         A: "Server",
         B: "ISP",
         C: "Browser",
         D: "All of the above.",
-        correct: C
-        },
-        //Question 9, index 8
-        {question: "Which of the following JavaScript statements displays 'Hello World' in an alert box?",
+        correct: "C",
+        type: "Easy"
+        }),
+    new Question({   
+        id: 9, 
+        question: "Which of the following JavaScript statements displays 'Hello World' in an alert box?",
         A: "alerttext(“HelloWorld!”);",
         B: "alertmsg(“HelloWorld!”);",
         C: "msgbox(“HelloWorld!”);",
         D: "alert(“HelloWorld!”);",
-        correct: D
-        },
-        //Question 10, index 9
-        {question: "What is the correct syntax for adding a comment in JavaScript?",
+        correct: "D",
+        type: "Difficult"
+        }),
+    new Question({
+        id: 10, 
+        question: "What is the correct syntax for adding a comment in JavaScript?",
         A: "//This is a comment",
         B: "<!–-This is a comment-->",
         C: "-*This is a comment",
         D: "!This is a comment",
-        correct: A
+        correct: "A",
+        type: "Medium"
+    }), ])
+
+    self.filterQuestionsById = ko.computed(function () {
+        if (!self.currentIndex()) {
+            return self.questions();
+        } else {
+            return ko.utils.arrayFilter(self.questions(), function (ques) {
+                return ques.id == self.currentIndex();
+            });
         }
-    ]);
+    });
 
-    self.addQuestion = function(){                   
-        return self.questions[self.currentRecord].question;                   
+    self.filter = function(idNum) {
+        self.currentIndex(idNum);
     }
-    
+}
 
-};
-          
-ko.applyBindings(new QuestionViewModel());
+ko.applyBindings(new QuizModel())
